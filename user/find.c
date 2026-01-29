dd9a487a9317cbe279eb996c5e8048cd9b8a6448
kate <czheng17@students.cs.ubc.ca>
2026-01-28 22:56:28 -0800

    test find

---
+++ user/find.c
@@ -0,0 +1,115 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+char*
+fmtname(char *path)
+{
+  static char buf[DIRSIZ + 1];
+  char *p;
+
+  // Find first character after last slash.
+  for (p = path + strlen(path); p >= path && *p != '/'; p--)
+    ;
+  p++;
+
+  // Return blank-padded name.
+  if (strlen(p) >= DIRSIZ)
+    return p;
+
+  memmove(buf, p, strlen(p));
+  memset(buf + strlen(p), ' ', DIRSIZ - strlen(p));
+  return buf;
+}
+
+void
+find(char *path, char *target)
+{
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+  if ((fd = open(path, 0)) < 0) {
+    fprintf(2, "find: cannot open %s\n", path);
+    return;
+  }
+
+  if (fstat(fd, &st) < 0) {
+    fprintf(2, "find: cannot stat %s\n", path);
+    close(fd);
+    return;
+  }
+
+  // 起点本身是文件的话，也要匹配
+  if (st.type == T_FILE) {
+    if (strcmp(fmtname(path), target) == 0) {
+      printf('%s\n', path);
+    }
+    close(fd);
+    return
+  }
+
+  if (st.type == T_DIR) {
+    if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf) {
+      printf("find: path too long\n");
+      close(fd);
+      return
+    }
+
+    strcpy(buf, path);
+    p = buf + strlen(buf);
+    *p++ = '/';
+
+    while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+      if (de.inum == 0)
+        continue;
+
+      // 取出项目名字（de.name)
+      char name[DIRSIZ + 1];
+      memmove(name, de.name, DIRSIZ);
+      name[DIRSIZ] = 0;
+
+      if (strcmp(name, '.') == 0 || stcmp(name, '..') == 0) {
+        continue;
+      }
+
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0
+
+      if (stat(buf, &st) < 0) {
+        printf("find: cannot stat %s\n", buf);
+        continue;
+      }
+
+      if (strcmp(name, target) == 0) {
+        printf('%s\n', buf);
+      }
+
+      if (st.type == T_DIR) {
+        find(buf, target);
+      }
+    }
+  }
+
+  close(fd);
+}
+
+int
+main(int argc, char *argv[])
+{
+  if (argc == 1) {
+    printf("incorrect command\n");
+  }
+
+  if (argc == 2) {
+    find(".", argv[1]);
+    exit(0);
+  }
+
+  if (argc == 3) {
+    find(argv[1], argv[2]);
+    exit(0);
+  }
+}
