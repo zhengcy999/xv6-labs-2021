1d0f939b718de0d2e2058b7d20f17ce07b5d4860
kate <czheng17@students.cs.ubc.ca>
2026-01-31 16:38:15 -0800

    add sysproc and syscall.c lab syscall

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -95,3 +95,13 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+//Add a sys_trace() function in kernel/sysproc.c 
+uint64
+sys_trace(void){
+  int mask;
+  if(argint(0, &mask) < 0)
+    return -1;
+  //myproc()->tracemask=mask;
+  return 0;
+}
