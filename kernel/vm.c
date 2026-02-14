154bbda2d49e3c48a5f2c4d966b9894ea802dc6e
kate <czheng17@students.cs.ubc.ca>
2026-02-13 16:21:19 -0800

    test

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,31 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+
+//todo vmprint()
+
+static void
+vmprint_rec(pagetable_t pagetable, int depth)
+{
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V){
+      for (int d=0;d<depth;d++){
+        printf(" ..");
+      }
+      uint64 pa=PTE2PA(pte);
+      printf("%d: pte %p pa %p\n", i,(void *)pte,(void *)pa);
+      if (pte & (PTE_R|PTE_W|PTE_X)==0){
+        vmprint_rec(pte,depth+1);
+      }
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable){
+  printf("page table %p\n", pagetable);
+  vmprint_rec(pagetable,1); //  start at deep 1
+}
