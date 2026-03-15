d0103fee7d24aa148727ea8bba8d75a3bb137583
kate <czheng17@students.cs.ubc.ca>
2026-03-14 19:38:30 -0700

    test traps

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -58,6 +58,8 @@ sys_sleep(void)
   int n;
   uint ticks0;
 
+  backtrace();
+
   if(argint(0, &n) < 0)
     return -1;
   acquire(&tickslock);
@@ -69,7 +71,6 @@ sys_sleep(void)
     }
     sleep(&ticks, &tickslock);
   }
-  backtrace();
   release(&tickslock);
   return 0;
 }
