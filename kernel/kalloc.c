21b33852d06e3afeea281935b3011d89e97b6c7e
kate <czheng17@students.cs.ubc.ca>
2026-03-18 17:25:47 -0700

    test cow

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -44,9 +44,10 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
     ref.count[PA2IDX(p)]=1;
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -96,12 +97,13 @@ kalloc(void)
   if(r)
     kmem.freelist = r->next;
   release(&kmem.lock);
-  if(r)
+  if(r){
     memset((char*)r, 5, PGSIZE); // fill with junk
 
     acquire(&ref.lock);
     ref.count[PA2IDX(r)]=1;
     release(&ref.lock);
+  }
 
   return (void*)r;
 }
