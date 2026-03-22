f2fc254a39be3726678f4917171f913c48ab7c1f
kate <czheng17@students.cs.ubc.ca>
2026-03-22 13:05:39 -0700

    test multithread

--- user/uthread.c
+++ user/uthread.c
@@ -10,10 +10,28 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct thread_context{
+  uint64 ra;
+  uint64 sp;
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct thread_context context;
+  
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -57,11 +75,14 @@ thread_schedule(void)
   if (current_thread != next_thread) {         /* switch threads?  */
     next_thread->state = RUNNING;
     t = current_thread;
+    t->state = RUNNABLE;
     current_thread = next_thread;
     /* YOUR CODE HERE
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)&t->context, (uint64)&current_thread->context);
+
   } else
     next_thread = 0;
 }
@@ -71,11 +92,17 @@ thread_create(void (*func)())
 {
   struct thread *t;
 
+
   for (t = all_thread; t < all_thread + MAX_THREAD; t++) {
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  memset(&t->context, 0, sizeof(t->context));
+  //save ra and sp
+  t->context.ra=(uint64)func;
+  t->context.sp=(uint64)t->stack+STACK_SIZE;
+
 }
 
 void 
