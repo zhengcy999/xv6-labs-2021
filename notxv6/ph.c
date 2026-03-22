4a128b1b79e89ffd227700133aa74e48eb76e7af
kate <czheng17@students.cs.ubc.ca>
2026-03-22 16:28:21 -0700

    test multithread

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -8,6 +8,8 @@
 #define NBUCKET 5
 #define NKEYS 100000
 
+pthread_mutex_t locks[NBUCKET]; 
+
 struct entry {
   int key;
   int value;
@@ -40,7 +42,7 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
-
+  pthread_mutex_lock(&locks[i]);
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -54,6 +56,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&locks[i]);
 
 }
 
@@ -105,13 +108,17 @@ main(int argc, char *argv[])
   void *value;
   double t1, t0;
 
-
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
     exit(-1);
   }
   nthread = atoi(argv[1]);
   tha = malloc(sizeof(pthread_t) * nthread);
+
+  for (int i = 0; i < NBUCKET; i++) {
+      pthread_mutex_init(&locks[i], NULL);
+  }
+  
   srandom(0);
   assert(NKEYS % nthread == 0);
   for (int i = 0; i < NKEYS; i++) {
