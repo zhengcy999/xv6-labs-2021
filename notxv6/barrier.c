7f07917b5efefd3cd83cf7d63ac11fb8a1e6280c
kate <czheng17@students.cs.ubc.ca>
2026-03-22 21:56:58 -0700

    test multithread

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,19 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  int my_round=bstate.round;
+  bstate.nthread++;
+  if (bstate.nthread==nthread){
+    bstate.round++;
+    bstate.nthread=0;
+    pthread_cond_broadcast(&bstate.barrier_cond);  
+  }else{
+    while (my_round==bstate.round){
+      pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex); 
+    }
+  }
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
